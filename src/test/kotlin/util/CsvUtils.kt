440e42694f92fa9ff6aea2f5e7f1e9d53e415f03
kamilchulakov <mr1english4@gmail.com>
2023-05-13 15:14:40 +0300

    Store values with headers

--- src/test/kotlin/util/CsvUtils.kt
+++ src/test/kotlin/util/CsvUtils.kt
@@ -4,6 +4,7 @@ object CsvUtils {
     fun readCsvFileValues(fileName: String): Map<Double, Double> =
         javaClass.getResourceAsStream(fileName)?.bufferedReader().use { reader ->
             val res = mutableMapOf<Double, Double>()
+            reader?.readLine() // Headers
             reader?.forEachLine { line ->
                 val (x, y) = line.split(",").map { it.toDouble() }
                 res[x] = y
