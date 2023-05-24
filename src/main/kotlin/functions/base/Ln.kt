fdb763aa1659d5ba6edfe5ecdbfca959d50f5bc3
kamilchulakov <mr1english4@gmail.com>
2023-05-24 18:48:34 +0300

    Improve ln

--- src/main/kotlin/functions/base/Ln.kt
+++ src/main/kotlin/functions/base/Ln.kt
@@ -1,69 +1,44 @@
 package functions.base
 
 import java.io.BufferedWriter
-import java.math.BigDecimal
-import kotlin.math.abs
+import kotlin.math.absoluteValue
 import kotlin.math.pow
-
 
 class Ln(
     private val csvLogger: BufferedWriter? = null
 ) : (Double, Double) -> Double {
-    override fun invoke(x: Double, eps: Double): Double {
-        val result = when {
-            x.isNaN() || x < 0 -> Double.NaN
-            x == 0.0 -> Double.NEGATIVE_INFINITY
-            x == 1.0 -> 0.0
-            x <= 2 -> calcLnByMercatorSeries(x - 1, eps)
-            x == Double.POSITIVE_INFINITY -> Double.POSITIVE_INFINITY
-            else -> calcLnEasy(x, eps)
-        }
-        return result.also {
-            csvLogger?.write("$x,$result")
+    override fun invoke(x: Double, eps: Double): Double =
+        calcThatWorks(x, eps).also {
+            csvLogger?.write("$x,$it")
             csvLogger?.newLine()
             csvLogger?.flush()
         }
-    }
 
     /**
-     * [wiki](https://ru.wikipedia.org/wiki/%D0%A0%D1%8F%D0%B4_%D0%9C%D0%B5%D1%80%D0%BA%D0%B0%D1%82%D0%BE%D1%80%D0%B0)
+     * based on
+     * https://mathsolver.microsoft.com/ru/solve-problem/%60log_%7B%20e%20%20%7D(%7B%20-1%20%20%7D)
      */
-    private fun calcLnByMercatorSeries(x: Double, eps: Double): Double {
-        val decX = BigDecimal.valueOf(x)
-        val decEps = BigDecimal.valueOf(eps)
-        var i = 1
-        var prev = BigDecimal.valueOf(0.0)
-        var curr = BigDecimal.valueOf(0.0)
-        var diff = BigDecimal.valueOf(Double.MAX_VALUE)
-        var pref = BigDecimal.valueOf(-1.0)
-        while (diff > decEps) {
-            pref *= BigDecimal.valueOf(-1.0)
-            curr += pref.div(BigDecimal.valueOf(1.0 * i)).times(decX.pow(i))
-            diff = (curr - prev).abs()
-            prev = curr
-            i++
+    private fun calcThatWorks(x: Double, eps: Double): Double {
+        val fastResult = when {
+            x == Double.POSITIVE_INFINITY -> Double.POSITIVE_INFINITY
+            x == 0.0 -> Double.NEGATIVE_INFINITY
+            x.isNaN() || x < 0.0 -> Double.NaN
+            else -> null
         }
-        return curr.toDouble()
-    }
+        if (fastResult != null)
+            return fastResult
 
-    /**
-     * https://wikimedia.org/api/rest_v1/media/math/render/svg/ffded915cdc900c0944340217624650781bc6fbd
-     * x = (1+x2)/(1-x2) => x-x*x2 = 1+x2 => x2*(-1-x)=-x+1 => x2=(-x+1)/(-1-x)=> x2=(x-1)/(1+x)
-     */
-    private fun calcLnEasy(x: Double, eps: Double): Double {
-        val x2 = (x-1).div(1+x)
-        var i = 0
-        var prev = 0.0
-        var curr = 0.0
-        var diff = Double.MAX_VALUE
-        while (diff > eps) {
-            val j = 2*i+1
-            curr += x2.pow(j).div(j)
-            diff = abs(curr - prev)
-            prev = curr
-            i++
+        val const = (x - 1).pow(2) / (x + 1).pow(2)
+        var ans = 0.0
+        var cur = (x - 1) / (x + 1)
+        var step = 1
+
+        while (cur.absoluteValue > eps) {
+            ans += cur
+            cur = (2 * step - 1) * cur * const / (2 * step + 1)
+            step++
         }
-        return curr*2
-    }
 
+        return ans * 2
+    }
 }
--- src/test/kotlin/modules/LnTest.kt
+++ src/test/kotlin/modules/LnTest.kt
@@ -2,17 +2,29 @@ package modules
 
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.data.Offset
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvFileSource
+import kotlin.math.ln
 
 class LnTest {
     private val ln = BaseModuleImpl().ln
-    private val eps = 0.0001
-    private val offset = Offset.offset(0.01)
+    private val eps = 0.00000000001
+    private val offset = Offset.offset(0.000000001)
 
     @ParameterizedTest
     @CsvFileSource(resources = ["/ln.csv"], useHeadersInDisplayName = true)
     fun testByTable(x: Double, result: Double) {
         assertThat(ln(x, eps)).isCloseTo(result, offset)
+    }
+
+    @TestFactory
+    fun testByStLib(): Collection<DynamicTest> = (-1000..1000).toList().map {
+        val x = it / 10.0
+        dynamicTest("$x, ln($x), eps=$offset") {
+            assertThat(ln(x, eps)).isCloseTo(ln(x), offset)
+        }
     }
 }
