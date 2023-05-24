49bda7f5d4de4c1a7e12eeb1e1f15b47f76baf06
kamilchulakov <mr1english4@gmail.com>
2023-05-24 19:37:49 +0300

    Add god system test

--- src/test/kotlin/functions/GodTest.kt
+++ src/test/kotlin/functions/GodTest.kt
@@ -3,16 +3,15 @@ package functions
 import functions.base.Cos
 import functions.base.Ln
 import functions.system.Cot
+import functions.system.Log
 import functions.system.Sin
 import functions.system.Tan
+import modules.SystemModuleImpl
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.data.Offset
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
-import kotlin.math.cos
-import kotlin.math.ln
-import kotlin.math.sin
-import kotlin.math.tan
+import kotlin.math.*
 
 class GodTest {
     private val eps = 0.00000000001
@@ -66,6 +65,35 @@ class GodTest {
             val x = it / 10.0
             DynamicTest.dynamicTest("$x, tan($x), eps=$cotOffset") {
                 assertThat(cot(x, eps)).isCloseTo(cos(x) / sin(x), cotOffset)
+            }
+        }
+    }
+
+    @TestFactory
+    fun testSystemByStLib(): Collection<DynamicTest> {
+        val libOffset = offset.value * 1000
+        val systemModuleImplByStLib = SystemModuleImpl(
+            cos = { x, _ -> cos(x) },
+            ln = { x, _ -> ln(x) },
+            cot = { x, _ -> cos(x) / sin(x) },
+            tan = { x, _ -> tan(x) },
+            sin = { x, _ -> sin(x) },
+            log = { base, x, _ -> log(x, base) }
+        )
+        val systemModuleImpl = SystemModuleImpl(
+            cos = Cos(),
+            ln = Ln(),
+            cot = Cot(),
+            tan = Tan(),
+            sin = Sin(),
+            log = Log()
+        )
+        return range.map {
+            val x = it / 10.0
+            DynamicTest.dynamicTest("$x, fun($x), eps=$libOffset") {
+                assertThat(
+                    (systemModuleImpl.system(x, eps) - systemModuleImplByStLib.system(x, eps))
+                            / systemModuleImplByStLib.system(x, eps)).isLessThanOrEqualTo(libOffset)
             }
         }
     }
