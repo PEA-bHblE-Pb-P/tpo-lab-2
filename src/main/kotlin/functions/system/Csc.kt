e56869bff56644b8171c4aa4140231283974ca22
kamilchulakov <mr1english4@gmail.com>
2023-05-24 17:18:46 +0300

    Rollback Csc

---
+++ src/main/kotlin/functions/system/Csc.kt
@@ -0,0 +1,25 @@
+@file:Suppress("MagicNumber")
+package functions.system
+
+import functions.base.Cos
+import java.io.BufferedWriter
+import kotlin.math.abs
+
+class Csc(
+    private val sin: (Double, Double) -> Double = Sin(Cos()),
+    private val csvLogger: BufferedWriter? = null
+) : (Double, Double) -> Double {
+    override fun invoke(x: Double, eps: Double): Double {
+        val sinVal = sin(x, eps * 0.1)
+        val result = if (sinVal.isNaN() || abs(sinVal) < eps) {
+            Double.NaN
+        } else {
+            1.0 / sinVal
+        }
+        return result.also {
+            csvLogger?.write("$x,$result")
+            csvLogger?.newLine()
+            csvLogger?.flush()
+        }
+    }
+}
