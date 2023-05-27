3ee79461de87615bebfea467c5ec66ebba42395c
kamilchulakov <mr1english4@gmail.com>
2023-05-27 13:58:59 +0300

    Fix custom assert

--- src/test/kotlin/util/Assertions.kt
+++ src/test/kotlin/util/Assertions.kt
@@ -3,10 +3,11 @@ package util
 import org.assertj.core.api.AbstractDoubleAssert
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.data.Offset
+import kotlin.math.abs
 
 fun assertByOffset(actual: Double, expected: Double, offset: Offset<Double>): AbstractDoubleAssert<*> {
     return if (actual.isNaN() || expected.isNaN())
         assertThat(actual).isCloseTo(expected, offset)
-    else assertThat((actual - expected) / expected)
+    else assertThat(abs((actual - expected) / (if (expected == 0.0) 1.0 else expected )))
         .isLessThanOrEqualTo(offset.value)
 }
