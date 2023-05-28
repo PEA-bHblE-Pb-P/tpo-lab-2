76b7fc37445b4019724ae173a71036ff6e74a23f
deevroman <roman.deev06@gmail.com>
2023-05-28 03:42:12 +0300

    fix plots when x unordered

--- src/gen_plot.py
+++ src/gen_plot.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-df = pd.read_csv(sys.argv[1], delimiter=',')
+df = pd.read_csv(sys.argv[1], delimiter=',').sort_values(by=['x'])
 print(df)
 plt.plot(df[df.columns[0]], df[df.columns[1]])
 plt.gcf().autofmt_xdate()
